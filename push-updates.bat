da40d66948afc54263604a3164cdf09c348ca658
Sondre <sondreee@gmail.com>
2025-05-02 12:02:02 +0200

    Initial commit: Fri 02/05/2025 12:02:02.52

--- push-updates.bat
+++ push-updates.bat
@@ -4,6 +4,6 @@ echo Initializing and pushing to GitHub...
 "C:\Program Files\Git\cmd\git.exe" add .
 "C:\Program Files\Git\cmd\git.exe" commit -m "Initial commit: %DATE% %TIME%"
 "C:\Program Files\Git\cmd\git.exe" branch -M main
-"C:\Program Files\Git\cmd\git.exe" remote add origin https://github.com/sondreee/kmk.git
+"C:\Program Files\Git\cmd\git.exe" remote add origin https://github.com/proteinnerd/kmk.git
 "C:\Program Files\Git\cmd\git.exe" push -u origin main
 echo Done! 
